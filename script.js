e3d5db6c135bc1432cc4d742df612f9cd7a95c1a
Joseph Godoy <josephgodoy.t@gmail.com>
2026-01-11 15:58:13 -0600

    Fin interfaz grafica

--- script.js
+++ script.js
@@ -1,52 +1,71 @@
+const btnPiedra = document.querySelector(".piedra")
+const btnPapel = document.querySelector(".papel")
+const btnTijeras = document.querySelector(".tijeras")
+const imagen = document.querySelector(".imgCPU")
+const mensaje = document.querySelector(".mensaje")
+const puntosCPU = document.querySelector(".puntosCPU")
+const puntosHumano = document.querySelector(".puntosJugador")
+const btnReinicio = document.querySelector(".reinicio")
+let puntosHumanoTmp = 0;
+let puntosCPUTmp = 0;
+let humanChoice = "";
+
+btnPiedra.addEventListener("click", function() {
+    humanChoice = 'piedra'
+    playRound(humanChoice, getComputerChoice());
+});
+
+btnPapel.addEventListener("click", function() {
+    humanChoice = 'papel'
+    playRound(humanChoice, getComputerChoice());
+});
+
+btnTijeras.addEventListener("click", function() {
+    humanChoice = 'tijeras'
+    playRound(humanChoice, getComputerChoice());
+});
+
+btnReinicio.addEventListener("click", function() {
+    mensaje.textContent = "Elige un objeto para jugar"
+    imagen.src = "img/nada.png"
+    puntosCPU.textContent = 0;
+    puntosHumano.textContent = 0;
+});
+
+
 function getComputerChoice() {
     let b = ''
     let a = Math.ceil(Math.random()*3);
     (a == 1) ? b = 'piedra' :
     (a == 2) ? b = 'papel' :
     b = 'tijeras'
+
+    switch (b) {
+        case 'piedra':
+            imagen.src = "img/piedra.png"
+            break;
+        case 'papel':
+            imagen.src = "img/Papel.webp"
+            break;
+        case 'tijeras':
+            imagen.src = "img/tijeras.png"
+            break;
+    }
     return b;
 }
 
-let getHumanChoice = () => prompt("Ingrese Piedra, papel o tijeras: ")
-
-function playGame() {
-
-    let humanScore = 0, computerScore = 0;
-    
-    function playRound(humanChoice, computerChoice) {
-            humanChoice = humanChoice.toLowerCase();
+function playRound(humanChoice, computerChoice) {
             if (humanChoice == computerChoice) {
-                console.log('Eleccion computadora: ' + computerChoice);
-                console.log('Tu eleccion: ' + humanChoice)
-                console.log("Empate")
-                console.log("")
+                mensaje.textContent = "Empate!"
             } else if (humanChoice == 'piedra' && computerChoice == 'tijeras' || humanChoice == 'tijeras' && computerChoice == 'papel' || humanChoice == 'papel' && computerChoice == 'piedra') {
-                console.log('Eleccion computadora: ' + computerChoice);
-                console.log('Tu eleccion: ' + humanChoice)
-                console.log("Punto para el humano")
-                console.log("")
-                humanScore = humanScore + 1;
+                mensaje.textContent = "Ganaste!"
+                puntosHumanoTmp = parseInt(puntosHumano.textContent);
+                puntosHumanoTmp = puntosHumanoTmp + 1;
+                puntosHumano.textContent = puntosHumanoTmp
             } else {
-                console.log('Eleccion computadora: ' + computerChoice);
-                console.log('Tu eleccion: ' + humanChoice)
-                console.log("Punto para la computadora")
-                console.log("")
-                computerScore = computerScore + 1;
+                mensaje.textContent = "Perdiste!"
+                puntosCPUTmp = parseInt(puntosCPU.textContent);
+                puntosCPUTmp = puntosCPUTmp + 1;
+                puntosCPU.textContent = puntosCPUTmp
             }
-        }
-
-    for (let i = 0; i <= 4; i++) { playRound(getHumanChoice(), getComputerChoice());} ;
-
-    console.log("Resultados:");
-    console.log('Computadora puntaje: ' + computerScore)
-    console.log('Humano puntaje: ' + humanScore)
-    if (computerScore > humanScore) {
-        console.log("Gana la computadora!") 
-    } else if (computerScore == humanScore) {
-        console.log("Empate!")
-    } else {
-        console.log("Gana el humano!");
-    }
 }
-
-playGame();
